a515ce9d4c237b7f2a8ef7474f550bdea38c4d21
Quang-Hieu Pham <pqhieu1192@gmail.com>
2015-12-18 20:15:48 +0800

    add constexpr keyword for c++11

--- config/c-c++-setup.el
+++ config/c-c++-setup.el
@@ -2,5 +2,6 @@
 (setq c-default-style "stroustrup")
 
 (font-lock-add-keywords 'c++-mode '(("constexpr" . font-lock-keyword-face)))
+(font-lock-add-keywords 'c++-mode '(("nullptr" . font-lock-keyword-face)))
 
 (provide 'c-c++-setup)
