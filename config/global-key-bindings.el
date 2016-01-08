50bb7edec5d5014d4b9529fbd049b26e0420d4ec
Quang-Hieu Pham <pqhieu1192@gmail.com>
2016-01-08 16:27:49 +0800

    make agenda full screen

--- config/global-key-bindings.el
+++ config/global-key-bindings.el
@@ -1,7 +1,8 @@
 (defun show-agenda-all ()
   ;; Show agenda and todo list
   (interactive)
-  (org-agenda nil "n"))
+  (org-agenda nil "n")
+  (delete-other-windows))
 
 (global-set-key (kbd "C-c a") 'show-agenda-all)
 
