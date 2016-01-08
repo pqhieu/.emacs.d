700d8f776e64acbcf516b4b87a38fcf95e1c4f8b
Quang-Hieu Pham <pqhieu1192@gmail.com>
2016-01-08 16:56:42 +0800

    make helm window always at bottom

--- config/helm-setup.el
+++ config/helm-setup.el
@@ -9,7 +9,6 @@
 (global-set-key (kbd "C-x b") 'helm-mini)
 
 (setq helm-quick-update nil
-      helm-split-window-in-side-p t
       helm-M-x-fuzzy-match t
       helm-buffers-fuzzy-matching t
       helm-recentf-fuzzy-match t)
@@ -18,10 +17,16 @@
 
 ;; Configure helm-swoop
 (global-set-key (kbd "C-s") 'helm-swoop)
-(setq helm-swoop-split-with-multiple-windows nil
+(setq helm-swoop-split-with-multiple-windows t
       helm-swoop-split-direction 'split-window-vertically
       helm-swoop-move-to-line-cycle t)
 (define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
 (define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
+
+(add-to-list 'display-buffer-alist
+             `(,(rx bos "*helm" (* not-newline) "*" eos)
+               (display-buffer-in-side-window)
+               (inhibit-same-window . t)
+               (window-height . 0.4)))
 
 (provide 'helm-setup)
