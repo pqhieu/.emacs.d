568c8476005bf655a371d4d23372045b1e3cde0a
Quang-Hieu Pham <pqhieu1192@gmail.com>
2016-01-08 16:28:24 +0800

    configure helm-swoop

--- config/helm-setup.el
+++ config/helm-setup.el
@@ -1,5 +1,6 @@
 (require 'helm-config)
 (require 'helm)
+(require 'helm-swoop)
 
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
 
@@ -7,6 +8,20 @@
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x b") 'helm-mini)
 
-(setq helm-quick-update nil)
+(setq helm-quick-update nil
+      helm-split-window-in-side-p t
+      helm-M-x-fuzzy-match t
+      helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match t)
+
+(helm-mode 1)
+
+;; Configure helm-swoop
+(global-set-key (kbd "C-s") 'helm-swoop)
+(setq helm-swoop-split-with-multiple-windows nil
+      helm-swoop-split-direction 'split-window-vertically
+      helm-swoop-move-to-line-cycle t)
+(define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
+(define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
 
 (provide 'helm-setup)
