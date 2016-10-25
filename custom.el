ca0f244428666edbc5637d453ebf05188062be77
Quang-Hieu Pham <pqhieu1192@gmail.com>
2016-10-25 14:38:11 +0800

    Some minor tweaks

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (zenburn-theme use-package spaceline org-bullets nyan-mode modern-cpp-font-lock ivy exec-path-from-shell all-the-icons))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -1,18 +1,17 @@
-1;;----------------------------------------------------------------------
+;;----------------------------------------------------------------------
 ;; My Personal Emacs Configuration
 ;;
 ;; I will try to keep it clean and well-documented as much as possible,
 ;; but sometimes the laziness just get me...
-;; I don't guarantee it will work on any computer or any Emacs version,
-;; and will not hold any responsibilities for it.
-;; Current init time: 2.0 seconds (still acceptable)
+;; I won't guarantee it will work on any computer or any Emacs version,
+;; and will not hold any responsibility for it.
 
 ;;----------------------------------------------------------------------
 ;; Package Initialisation
 ;; Start Emacs package manager
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
 (setq package-enable-at-startup nil)
 (package-initialize)
 ;; Check for use-package and install if needed
@@ -24,26 +23,33 @@
 (require 'use-package)
 (setq use-package-verbose t)
 
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+;;----------------------------------------------------------------------
+;; User-defined Functions
+(defun show-agenda-all ()
+  ;; Show agenda and todo list
+  (interactive)
+  (org-agenda nil "n")
+  (delete-other-windows))
+
 ;;----------------------------------------------------------------------
 ;; General Settings
 ;; Set personal information
 (setq user-full-name "Quang-Hieu Pham")
 (setq user-mail-address "pqhieu1192@gmail.com")
-;; Disable backup files and auto save
-;; I like to live dangerously
+;; Disable backup and autosaving
 (setq-default make-backup-files nil)
 (setq-default backup-inhibited t)
 (setq-default auto-save-default nil)
 ;; Set tab width and its behaviour
-(setq-default tab-width 4)
+(setq default-tab-width 4)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-always-indent 'complete)
 ;; Insert new line at EOF when save
 (setq-default require-final-newline t)
-;; Ask the real question: 'y' or 'n'
 (fset 'yes-or-no-p 'y-or-n-p)
-;; Start in fullscreen
-(set-frame-parameter nil 'fullscreen 'fullboth)
 
 ;;----------------------------------------------------------------------
 ;; Display Settings
@@ -54,133 +60,74 @@
 (setq-default initial-scratch-message nil)
 ;; Highlight current line and show column number
 (column-number-mode 1)
-;; (global-hl-line-mode 1)
 ;; Uniquify buffer names
 (setq-default uniquify-buffer-name-style 'forward)
 ;; Highligt corresponding parentheses
-(show-paren-mode t)
+(show-paren-mode 1)
 ;; Set theme and font
-(set-frame-font "Source Code Pro-13")
-(load-theme 'foggy-night t)
-;; (use-package tao-theme
-;;   :ensure t
-;;   :init (load-theme 'tao-yin t))
-;; ;; TODO: Directly change these stuff instead of using Custom
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-meta-line ((t (:height 1.0))))
- '(org-scheduled-today ((t (:height 1.0)))))
-;;  '(org-target ((t (:foreground "white smoke" :slant italic)))))
+(set-frame-font "Fira Code-13")
+;; Start in fullscreen
+(set-frame-parameter nil 'fullscreen 'fullboth)
 
 ;;----------------------------------------------------------------------
-;; Helm Settings
-(use-package helm
-  :ensure t
-  :diminish (helm-mode . "Ⓗ")
+;; User-installed Package Settings
+;; Set Emacs theme
+(load-theme 'zenburn t)
+;; Modeline config
+(use-package spaceline
   :init
-  (require 'helm-config)
-  (require 'helm)
-  (bind-key "<tab>" 'helm-execute-persistent-action helm-map)
-  (helm-mode)
-  :bind (("M-x" . helm-M-x)
-	 ("C-x C-f" . helm-find-files)
-	 ("C-x b" . helm-mini)
-	 ("C-s" . helm-swoop)))
+  ;; Disable sRGB on Mac OSX to get a sharp look
+  (setq ns-use-srgb-colorspace nil)
+  (require 'spaceline-config)
+  (spaceline-emacs-theme))
+(use-package nyan-mode
+  :init (nyan-mode 1)
+  :config
+  (nyan-start-animation))
+;; Set up Ivy for completion
+(use-package ivy
+  :ensure t
+  :diminish ivy-mode
+  :config
+  (ivy-mode 1)
+  ;; Add recent files into completion list
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-height 10)
+  (setq ivy-count-format "")
+  (setq ivy-initial-inputs-alist nil))
+;; Org-mode settings
+(use-package org
+  :ensure t
+  :config
+  (setq org-agenda-todo-ignore-scheduled (quote all))
+  (setq org-agenda-todo-ignore-timestamp (quote all))
+  (setq org-agenda-files (list "~/Dropbox/Documents/org/general.org"))
+  (setq org-ellipsis "▼")
+  (global-set-key (kbd "C-c a") 'show-agenda-all))
+(use-package org-bullets
+  :config
+  (add-hook 'org-mode-hook 'org-bullets-mode 1))
 
 (defun my-c-setup ()
   (c-set-offset 'innamespace 0))
 (add-hook 'c++-mode-hook 'my-c-setup)
 
-(use-package yasnippet
-  :diminish 'yas-minor-mode
-  :init (yas-global-mode))
-
-(add-hook 'after-init-hook 'show-agenda-all)
 (exec-path-from-shell-initialize)
-
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
 (add-hook 'c++-mode-hook #'modern-c++-font-lock-mode)
 (setq modern-c++-literal-integer nil)
 
 (require 'whitespace)
-(setq whitespace-line-column 80) ;; limit line length
 (setq whitespace-style '(face lines-tail))
 
-(add-hook 'prog-mode-hook 'whitespace-mode)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-todo-ignore-scheduled (quote all))
- '(org-agenda-todo-ignore-timestamp (quote all))
- '(org-agenda-todo-list-sublevels nil)
- '(package-selected-packages
-   (quote
-    (glsl-mode org-page foggy-night-theme reykjavik-theme helm-swoop org-bullets modern-cpp-font-lock s helm)))
- '(powerline-default-separator (quote wave)))
-
-
-(setq font-lock-maximum-decoration 1)
 (require 'cc-mode)
-(setq c-default-style "stroustrup")
+(setq c-default-style "ellemtel")
+(setq c-basic-offset 4)
 (add-hook 'prog-mode-hook (lambda() (setq show-trailing-whitespace t)))
 
+(require 'dired )
+(define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file) ; was dired-advertised-find-file
+(define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file "..")))  ; was dired-up-directory
+(put 'dired-find-alternate-file 'disabled nil)
+
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-(setq ns-use-srgb-colorspace nil)
-(require 'spaceline-config)
-(spaceline-emacs-theme)
-
-(require 'nyan-mode)
-(nyan-mode t)
-(setq org-agenda-files (list "~/Dropbox/Documents/org/gtd.org"))
-
-(setq org-agenda-show-log t)
-(setq org-agenda-skip-scheduled-if-done t)
-(setq org-agenda-skip-deadline-if-done t)
-(setq org-agenda-skip-timestamp-if-done t)
-(setq org-agenda-start-on-weekday nil)
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-(defun show-agenda-all ()
-  ;; Show agenda and todo list
-  (interactive)
-  (org-agenda nil "n")
-  (delete-other-windows))
-
-(global-set-key (kbd "C-c a") 'show-agenda-all)
-
-(setq helm-quick-update nil
-      helm-split-window-in-side-p t
-      helm-M-x-fuzzy-match t
-      helm-buffers-fuzzy-matching t
-      helm-recentf-fuzzy-match t)
-(setq helm-swoop-split-with-multiple-windows t
-      helm-swoop-split-direction 'split-window-vertically
-      helm-swoop-move-to-line-cycle t)
-;; (define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
-;; (define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
-
-(add-to-list 'display-buffer-alist
-             `(,(rx bos "*helm" (* not-newline) "*" eos)
-               (display-buffer-in-side-window)
-               (inhibit-same-window . t)
-               (window-height . 0.4)))
-
-(global-set-key (kbd "C-c C-k") (lambda ()
-                               (interactive)
-                               (setq-local compilation-read-command nil)
-                               (call-interactively 'compile)))
-
-
-(require 'org-page)
-(setq op/repository-directory "~/Workspace/pandora")
-(setq op/site-domain "https://pqhieu.github.io")
-(setq op/theme-root-directory "~/Workspace/pandora/themes")
-(setq op/theme 'org-page-one-column)
-
-(setq org-ellipsis "▼")
