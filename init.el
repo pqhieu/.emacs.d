fa736827309bf45b71f42d861024af2fb33a1c1d
Quang-Hieu Pham <pqhieu1192@gmail.com>
2017-08-16 10:14:44 +0800

    Change font to Fira Code, and ignore tramp

--- init.el
+++ init.el
@@ -68,7 +68,7 @@
 ;; Highligt corresponding parentheses
 (show-paren-mode 1)
 ;; Set theme and font
-(set-frame-font "Source Code Pro-14")
+(set-frame-font "Fira Code-14")
 ;; Set Emacs theme
 (load-theme 'gruvbox-dark-hard t)
 ;; Set recenter command behaviour
