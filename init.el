7d524949b49d98141db15350424cb5ac2afbee57
Quang-Hieu Pham <pqhieu1192@gmail.com>
2019-03-11 13:54:32 +0800

    Change theme in to doom-sourcerer

--- init.el
+++ init.el
@@ -78,7 +78,7 @@
 (use-package doom-themes
   :ensure t
   :config
-  (load-theme 'doom-tomorrow-night t)
+  (load-theme 'doom-sourcerer t)
   (setq doom-themes-enable-bold t)
   (setq doom-themes-enable-italic t)
   (doom-themes-visual-bell-config)
