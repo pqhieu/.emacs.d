40e940ecaaa4e62c8a9eb77bb705e4a9f8f15e8c
Quang-Hieu Pham <pqhieu1192@gmail.com>
2020-03-13 23:49:26 -0700

    Overhaul my configuration

--- init.el
+++ init.el
@@ -1,21 +1,80 @@
-;;----------------------------------------------------------------------
-;; My personal Emacs configuration
+;; init.el --- Quang-Hieu's personal Emacs configuration
 ;;
 ;; Copyright (C) 2015-2020
 ;;
 ;; Author: Quang-Hieu Pham <pqhieu1192@gmail.com>
 ;;
-;; I will try to keep it clean and well-documented as much as possible,
-;; but sometimes the laziness just get me...
-;; I won't guarantee it will work on any computer or any Emacs version,
-;; and will not hold any responsibility for it.
+;; License:
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
+;; Reduce the frequency of garbage collection by making it happen on
+;; each 50MB of allocated data (the default is on every 0.76MB)
+(setq gc-cons-threshold 50000000)
+(setq file-name-handler-alist-original file-name-handler-alist)
+(setq file-name-handler-alist nil)
 ;; Start Emacs package manager
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
-(setq package-enable-at-startup nil)
-(package-initialize nil)
+(package-initialize)
+;; Set personal information
+(setq user-full-name "Quang-Hieu Pham")
+(setq user-mail-address "pqhieu1192@gmail.com")
+;; Disable splash screen, toolbar and scrollbar
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(setq-default inhibit-splash-screen t)
+(setq-default initial-scratch-message nil)
+;; Disable backup and auto-saving
+(setq-default make-backup-files nil)
+(setq-default backup-inhibited t)
+(setq-default auto-save-default nil)
+;; Disable blinking cursor
+(blink-cursor-mode 0)
+(setq-default cursor-type 'bar)
+;; Disable the annoying bell ring
+(setq ring-bell-function 'ignore)
+;; Better scrolling
+(setq scroll-margin 0)
+(setq scroll-conservatively 100000)
+(setq scroll-preserve-screen-position 1)
+;; Set tab width and its behavior
+(setq default-tab-width 4)
+(setq-default indent-tabs-mode nil)
+(setq-default fill-column 80)
+(setq-default tab-always-indent 'complete)
+;; Insert new line at EOF when save
+(setq-default require-final-newline t)
+(fset 'yes-or-no-p 'y-or-n-p)
+;; Show line and column numbers in mode line
+(line-number-mode t)
+(column-number-mode t)
+;; Revert buffers automatically
+(global-auto-revert-mode t)
+;; Display line numbers
+(global-display-line-numbers-mode t)
+;; Highlight corresponding parentheses
+(show-paren-mode 1)
+;; Handle camel case
+(add-hook 'prog-mode-hook #'subword-mode)
+;; Disable all changes through customize
+(setq custom-file (make-temp-file ""))
+
 ;; Check for use-package and install if needed
 (unless (package-installed-p 'use-package)
   (message "`use-package` not found. Installing...")
@@ -23,110 +82,48 @@
   (package-install 'use-package))
 ;; Config use-package
 (require 'use-package)
+(require 'bind-key)
 (setq use-package-verbose t)
-(use-package diminish :ensure t)
-
-;; Set personal information
-(setq user-full-name "Quang-Hieu Pham")
-(setq user-mail-address "pqhieu1192@gmail.com")
-;; Disable backup and auto-saving
-(setq-default make-backup-files nil)
-(setq-default backup-inhibited t)
-(setq-default auto-save-default nil)
-;; Set tab width and its behaviour
-(setq default-tab-width 4)
-(setq-default indent-tabs-mode nil)
-(setq-default tab-always-indent 'complete)
-(setq-default fill-column 79)
-;; Insert new line at EOF when save
-(setq-default require-final-newline t)
-(fset 'yes-or-no-p 'y-or-n-p)
-(setq custom-file (make-temp-file ""))
-(setq display-time-day-and-date t)
-
-;; Disable splash screen, toolbar and scrollbar
-(menu-bar-mode 0)
-(tool-bar-mode 0)
-(scroll-bar-mode 0)
-(blink-cursor-mode 0)
-(display-time-mode 1)
-(setq-default cursor-type 'bar)
-(setq-default inhibit-splash-screen t)
-(setq-default initial-scratch-message nil)
-;; Show column number
-(column-number-mode 1)
+;; Set default font and line spacing
+(add-to-list 'default-frame-alist '(font . "Atlas Typewriter-13"))
+(setq-default line-spacing 0.3)
 ;; Uniquify buffer names
-(setq-default uniquify-buffer-name-style 'forward)
-;; Highlight corresponding parentheses
-(show-paren-mode 1)
-;; Disable bell
-(setq ring-bell-function 'ignore)
-;; Set font
-(set-frame-font "IBM Plex Mono:pixelsize=13:weight=normal:slant=normal:width=normal:spacing=100:scalable=true")
-(setq-default line-spacing 0.2)
-;; Set theme
-(use-package doom-themes
-  :ensure t
+(use-package uniquify
   :config
-  (setq doom-themes-enable-bold t)
-  (setq doom-themes-enable-italic t)
-  (load-theme 'doom-tomorrow-night t)
-  (doom-themes-org-config)
-  (set-face-attribute 'font-lock-comment-face nil :slant 'italic)
-  (set-face-attribute 'font-lock-keyword-face nil :slant 'italic)
-  (set-face-attribute 'font-lock-doc-face nil :slant 'italic)
-  (set-face-attribute 'font-lock-preprocessor-face nil :slant 'italic))
-;; Auto-revert buffers
-(global-auto-revert-mode 1)
-(global-linum-mode 1)
-
-(defun org-agenda-show-all ()
-  "Show both agenda and todo list."
-  (interactive)
-  (org-agenda nil "n")
-  (delete-other-windows))
-(defun kill-other-buffers ()
-  "Kill all other buffers."
-  (interactive)
-  (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
-
-(use-package swiper
-  :ensure t
-  :bind (("C-s" . swiper)))
-(use-package counsel
-  :ensure t
-  :diminish ivy-mode
-  :bind (("M-x" . counsel-M-x)
-         ("C-x C-f" . counsel-find-file)
-         ("C-c 8" . counsel-unicode-char)
-         ("C-c s" . counsel-git-grep)
-         ("C-h v" . counsel-describe-variable)
-         ("C-h f" . counsel-describe-function)
-         ("M-y" . counsel-yank-pop))
+  (setq uniquify-buffer-name-style 'forward)
+  (setq uniquify-separator "/")
+  ;; Rename after killing buffer
+  (setq uniquify-after-kill-buffer-p t)
+  ;; Ignore special buffers
+  (setq uniquify-ignore-buffers-re "^\\*"))
+;; Add recent files
+(use-package recentf
   :config
-  (ivy-mode 1)
-  (setq ivy-count-format "(%d/%d) ")
-  (setq ivy-use-virtual-buffers t) ;; Add recent files into completion list
-  (setq ivy-use-selectable-prompt t)
-  (setq ivy-display-style 'fancy)
-  (setq ivy-height 15)
-  (setq ivy-switch-buffer-faces-alist nil) ;; Remove '^' at the beginning
-  (setq ivy-initial-inputs-alist nil)
-  (setq counsel-find-file-ignore-regexp "\\(?:^Icon?\\)"))
-(use-package ivy-bibtex
-  :ensure t
-  :bind (("C-c b" . ivy-bibtex))
+  (setq recentf-max-saved-items 500)
+  (setq recentf-max-menu-items 15)
+  ;; Disable recentf cleanup on Emacs start, because it can cause
+  ;; problems with remote files
+  (setq recentf-auto-cleanup 'never))
+;; Use Shift + arrow keys to switch between buffers
+(use-package windmove
   :config
-  (setq bibtex-completion-bibliography '("~/Dropbox/main.bib"))
-  (setq bibtex-completion-notes-symbol "✎")
-  (setq bibtex-completion-cite-prompt-for-optional-arguments nil)
-  (setq bibtex-completion-format-citation-functions
-        '((org-mode . bibtex-completion-format-citation-org-title-link-to-PDF)
-          (latex-mode . bibtex-completion-format-citation-cite)
-          (markdown-mode . bibtex-completion-format-citation-pandoc-citeproc)
-          (default . bibtex-completion-format-citation-default)))
-  (setq ivy-bibtex-default-action 'ivy-bibtex-insert-citation))
-
+  (windmove-default-keybindings))
+(use-package dired
+  :config
+  ;; Enable dired-jump
+  (require 'dired-x)
+  (setq dired-recursive-copies 'always)
+  (setq dired-recursive-deletes 'always)
+  (setq delete-by-moving-to-trash t)
+  (setq dired-dwim-target t)
+  ;; Reuse current buffer by pressing 'a'
+  (put 'dired-find-alternate-file 'disabled nil)
+  (define-key dired-mode-map (kbd "^")
+    (lambda () (interactive) (find-alternate-file "..")))
+  ;; Remember to install coreutils on OSX
+  (if (eq system-type 'darwin)
+      (setq insert-directory-program "gls" dired-use-ls-dired t))
+  (setq dired-listing-switches "-aFhlv --group-directories-first"))
 (use-package org
   :config
   (setq org-ellipsis "⤵")
@@ -159,65 +156,14 @@
   (setq org-agenda-todo-ignore-scheduled (quote all))
   (setq org-agenda-todo-ignore-timestamp (quote all))
   (setq org-agenda-tags-column -77)
-  ;; do not show scheduled/deadline if done
+  ;; Do not show scheduled/deadline if done
   (setq org-agenda-skip-deadline-prewarning-if-scheduled t)
   (setq org-agenda-skip-deadline-if-done t)
   (setq org-agenda-skip-scheduled-if-done t)
-  ;; do not show repeating task
+  ;; Do not show repeating task
   (setq org-agenda-repeating-timestamp-show-all nil)
   (setq org-agenda-sorting-strategy
         '((agenda todo-state-up priority-down))))
-(use-package org-bullets
-  :ensure t
-  :hook (org-mode . org-bullets-mode)
-  :init
-  (setq org-bullets-bullet-list '("⓵" "⓶" "⓷" "⓸" "⓹" "⓺" "⓻" "⓼")))
-
-(use-package magit
-  :ensure t
-  :bind ("C-c g" . magit-status))
-(use-package dired
-  :config
-  (setq dired-recursive-copies 'always)
-  (setq dired-recursive-deletes 'always)
-  (setq delete-by-moving-to-trash t)
-  (setq dired-dwim-target t)
-  ;; reuse dired buffer
-  (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
-  (put 'dired-find-alternate-file 'disabled nil)
-  (define-key dired-mode-map (kbd "^")
-    (lambda () (interactive) (find-alternate-file "..")))
-  ;; remember to install coreutils on OSX
-  (if (eq system-type 'darwin)
-      (setq insert-directory-program "gls" dired-use-ls-dired t))
-  (setq dired-listing-switches "-aFhlv --group-directories-first"))
-(use-package beacon
-  :ensure t
-  :diminish beacon-mode
-  :init (beacon-mode 1))
-(use-package exec-path-from-shell
-  :ensure t
-  :init (exec-path-from-shell-initialize))
-(use-package doom-modeline
-  :ensure t
-  :hook (after-init . doom-modeline-init)
-  :config
-  (setq doom-modeline-major-mode-icon t)
-  (setq doom-modeline-buffer-file-name-style 'auto))
-(use-package ledger-mode
-  :ensure t
-  :mode ("\\.dat\\'")
-  :config
-  (setq ledger-clear-whole-transactions t))
-
-(use-package whitespace
-  :ensure t
-  :diminish whitespace-mode
-  :config
-  (setq whitespace-style (quote (face trailing tab-mark)))
-  ;; delete trailing whitespace when save
-  (add-hook 'before-save-hook 'delete-trailing-whitespace)
-  (add-hook 'prog-mode-hook 'whitespace-mode))
 (use-package cc-mode
   :config
   (defun c-setup ()
@@ -225,46 +171,125 @@
     (c-set-offset 'inextern-lang [0]))
   (add-hook 'c-mode-common-hook 'c-setup)
   (setq c-default-style "linux")
-  (setq c-basic-offset 4)
-  (add-to-list 'auto-mode-alist '("\\.cuh\\'" . c++-mode))
-  (add-to-list 'auto-mode-alist '("\\.cu\\'" . c++-mode)))
-(use-package clang-format :ensure t)
-(use-package glsl-mode :ensure t)
-(use-package yaml-mode :ensure t)
+  (setq c-basic-offset 4))
+(use-package whitespace
+  :ensure t
+  :config
+  (setq whitespace-line-column 88) ;; limit line length
+  (setq whitespace-style '(face tabs empty trailing lines-tail))
+  ;; Delete trailing whitespace when save
+  (add-hook 'before-save-hook #'whitespace-cleanup)
+  (add-hook 'prog-mode-hook #'whitespace-mode)
+  (add-hook 'text-mode-hook #'whitespace-mode))
+
+;; Load shell environment variables
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize)))
+;; Load color theme
+(use-package doom-themes
+  :ensure t
+  :config
+  (setq doom-themes-enable-bold t)
+  (setq doom-themes-enable-italic t)
+  (load-theme 'doom-tomorrow-night t)
+  (doom-themes-org-config))
+(use-package doom-modeline
+  :ensure t
+  :config
+  (doom-modeline-mode 1)
+  (setq doom-modeline-major-mode-color-icon t)
+  (setq doom-modeline-minor-modes nil))
+;; Ivy -- interactive interfact for completion
+(use-package ivy
+  :ensure t
+  :config
+  (ivy-mode 1)
+  (setq ivy-count-format "(%d/%d) ")
+  (setq ivy-use-virtual-buffers t) ;; add recent files into completion list
+  (setq ivy-use-selectable-prompt t)
+  (setq ivy-display-style 'fancy)
+  (setq ivy-height 15))
+(use-package swiper
+  :ensure t
+  :bind (("C-s" . swiper)))
+(use-package counsel
+  :after ivy
+  :ensure t
+  :bind (("M-x" . counsel-M-x)
+         ("C-x C-f" . counsel-find-file)
+         ("C-c l" . counsel-locate)
+         ("C-c 8" . counsel-unicode-char)
+         ("C-c s" . counsel-git-grep)
+         ("C-h v" . counsel-describe-variable)
+         ("C-h f" . counsel-describe-function)
+         ("M-y" . counsel-yank-pop))
+  :config
+  (setq counsel-find-file-ignore-regexp "\\(?:^Icon?\\)"))
+(use-package ivy-bibtex
+  :ensure t
+  :after ivy
+  :bind ("C-c b" . ivy-bibtex)
+  :config
+  (setq bibtex-completion-bibliography '("~/Dropbox/main.bib"))
+  (setq bibtex-completion-cite-prompt-for-optional-arguments nil)
+  (setq bibtex-completion-format-citation-functions
+        '((org-mode . bibtex-completion-format-citation-org-title-link-to-PDF)
+          (latex-mode . bibtex-completion-format-citation-cite)
+          (markdown-mode . bibtex-completion-format-citation-pandoc-citeproc)
+          (default . bibtex-completion-format-citation-default)))
+  (setq ivy-bibtex-default-action 'ivy-bibtex-insert-citation))
+(use-package magit
+  :ensure t
+  :bind ("C-c g" . magit-status))
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0.5)
+  (setq company-show-numbers t)
+  (setq company-tooltip-limit 10)
+  (setq company-minimum-prefix-length 2)
+  (setq company-tooltip-align-annotations t)
+  (setq company-tooltip-flip-when-above t)
+  (global-company-mode))
+(use-package latex
+  :defer t
+  :ensure auctex
+  :config
+  (setq TeX-parse-self t))
+(use-package org-bullets
+  :ensure t
+  :hook (org-mode . org-bullets-mode)
+  :init
+  (setq org-bullets-bullet-list '("⓵" "⓶" "⓷" "⓸" "⓹" "⓺" "⓻" "⓼")))
+(use-package clang-format :ensure t :defer t)
+(use-package glsl-mode :ensure t :defer t)
+(use-package yaml-mode :ensure t :defer t)
+(use-package ledger-mode
+  :ensure t
+  :defer t
+  :mode ("\\.dat\\'")
+  :config
+  (setq ledger-clear-whole-transactions t))
 (use-package markdown-mode
   :ensure t
   :config
   (setq markdown-fontify-code-blocks-natively t))
-(use-package subword
-  :ensure t
-  :diminish subword-mode
-  :config
-  (global-subword-mode 1))
 
-(use-package company
-  :ensure t
-  :diminish company-mode
-  :config
-  ;; disable prompt when adding local variables
-  (setq enable-local-variables :all)
-  (setq company-dabbrev-downcase nil)
-  (setq company-idle-delay 0.5)
-  (setq company-backends (delete 'company-semantic company-backends))
-  (define-key c-mode-map  (kbd "C-<return>") 'company-complete)
-  (define-key c++-mode-map  (kbd "C-<return>") 'company-complete)
-  (add-hook 'after-init-hook 'global-company-mode))
+(defun kill-other-buffers ()
+  "Kill all other buffers."
+  (interactive)
+  (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
+(defun org-agenda-show-all ()
+  "Show both agenda and todo list."
+  (interactive)
+  (org-agenda nil "n")
+  (delete-other-windows))
 
-(use-package latex
-  :ensure auctex
-  :config
-  (setq TeX-auto-save t)
-  (setq TeX-parse-self t)
-  ;; more sensible wrapping when writing
-  (add-hook 'LaTeX-mode-hook 'visual-line-mode))
-
-(global-set-key (kbd "C-c w") 'kill-other-buffers)
-(global-set-key (kbd "C-c f") 'toggle-frame-fullscreen)
-
-;; Show your agenda and make Emacs go fullscreen
-(add-hook 'after-init-hook 'org-agenda-show-all)
-(toggle-frame-fullscreen)
+(global-set-key (kbd "C-c \\") #'align-regexp) ;; align code based on regex
+(global-set-key (kbd "C-c k") #'kill-other-buffers)
+(global-set-key (kbd "C-c f") #'toggle-frame-fullscreen)
+(add-hook 'after-init-hook #'toggle-frame-fullscreen)
+(add-hook 'after-init-hook #'org-agenda-show-all)
